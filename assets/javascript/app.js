e95152405c547d6f5bd008e28a93de1b9d3b2c6f
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2017-12-14 10:04:15 -0500

    changed the name for the different questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,70 +4,107 @@ var secondsLeft = 10;
 var intervalId;
 
 var question1 = {question:"What is my name?",
- correct:"Elisheva",
-  incorrect:["Bob", "Charlie", "Lara"]};
+correct:"Elisheva",
+incorrect:["Bob", "Charlie", "Lara"]};
+
+var question2 = {question:"What is my name?",
+correct:"Elisheva",
+incorrect:["Bob", "Charlie", "Lara"]};
+
+var question3 = {question:"What is my name?",
+correct:"Elisheva",
+incorrect:["Bob", "Charlie", "Lara"]};
+
+var question4 = {question:"What is my name?",
+correct:"Elisheva",
+incorrect:["Bob", "Charlie", "Lara"]};
+
+var question5 = {question:"What is my name?",
+correct:"Elisheva",
+incorrect:["Bob", "Charlie", "Lara"]};
+
+var question6 = {question:"What is my name?",
+correct:"Elisheva",
+incorrect:["Bob", "Charlie", "Lara"]};
+
+var question7 = {question:"What is my name?",
+correct:"Elisheva",
+incorrect:["Bob", "Charlie", "Lara"]};
+
 
 var classArray = ["c", "i", "i", "i"];
 
- var form = $("form");
+var form = $("form");
 
- var quest = $("<p>").text(question1.question);
- 
+for (var j = 0; j < 7; j++) {
+
+// the question
+var quest = $("<p>").text((question1).question);
+var section = $("<section>");
+
 
 form.append("<br>");
-var radioButton = $("<div>");
-radioButton.addClass("radio corr");
-label = $("<label>");
-input = $("<input>");
-input.attr("type", "radio");
-input.attr("name", "optradio");
-input.attr("id", "question1choice1");
-
-label.append(input);
-
-label.append(question1.correct);
-radioButton.append(label)
-
-
-form.append(quest, radioButton);
+form.append(quest);
 
 
 //do again for other incorrect choices
-for (var i = 0; i < question1.incorrect.length; i++) {
+for (var i = 0; i <= question1.incorrect.length; i++) {
+
+	var radioButton = $("<div>");
+// section.append(radioButton);
+if (i == 0){
+	radioButton.addClass("radio corr");
+	label = $("<label>");
+	input = $("<input>");
+	input.attr("type", "radio");
+	input.attr("name", "optradio" + (i+1));
+	input.attr("id", "question"+(j+1)+"choice1");
+
+	label.append(input);
+
+	label.append(question1.correct);
+	radioButton.append(label)
+}
+
+else{
 	
 	radioButton = $("<div>");
+
 	radioButton.addClass("radio incorr");
 	label = $("<label>");
 	input = $("<input>");
 	input.attr("type", "radio");
-	input.attr("name", "optradio");
-	input.attr("id", "question1"+"choice" + (i));
+	input.attr("name", "optradio" + (i+1));
+	input.attr("id", "question"+(j+1)+"choice" + (i+1));
 
 	label.append(input);
 
-	label.append(question1.incorrect[i]);
+	
+
+	label.append(question1.incorrect[i-1]);
+
+	
 	radioButton.append(label)
-
-
-	form.append( radioButton);
 }
 
-// radioButton = $("<div>");
-// radioButton.addClass("radio");
-// label = $("<label>");
-// input = $("<input>");
-// input.attr("type", "radio");
-// input.attr("name", "optradio");
-
-// label.append(input);
-
-// radioButton.html(question1.correct);
-// radioButton.prepend(label)
+	//randomize the order of the choices
+	var rand = Math.floor(Math.random()*2);
+	console.log(i + "" + rand);
+	if (rand == 0){
 
 
-// form.append(quest, radioButton);
+		section.append(radioButton);
+	}
+	else{
+		section.prepend(radioButton);
+	}
 
+	
 
+	
+	form.append(section);
+}
+}
 
 
 function run(){
@@ -76,25 +113,25 @@ function run(){
 
 function decrement() {
 
-      secondsLeft--;
+	secondsLeft--;
 
-      $("#show-number").html("<h2>" + secondsLeft + "</h2>");
+	$("#show-number").html("<h2>" + secondsLeft + "</h2>");
 
-      if (secondsLeft === 0) {
+	if (secondsLeft === 0) {
 
-        stop();
+		stop();
 
-        console.log("Time Up!");
+		console.log("Time Up!");
 
         // need to give number this value again, becuase otherwise when time is up, if user hits return, it will decrement from 0
         // and display negaive numbers
         // secondsLeft = 100;
-      }
     }
+}
 
-    function stop() {
+function stop() {
 
-      clearInterval(intervalId);
-    }
+	clearInterval(intervalId);
+}
 
-    run();
+run();
