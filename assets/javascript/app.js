de9b2da9a1ec08b6101e0fe626c8fe69bc59eb60
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2017-12-14 19:44:05 -0500

    shows correct answers etc

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,59 @@
 
-var secondsLeft = 10;
+var secondsLeft = 100;
 
 var intervalId;
 
-var question1 = {question:"What is my name?",
-correct:"Elisheva",
-incorrect:["Bob", "Charlie", "Lara"]};
+var question1 = {question:"1. Thanksgiving occurs on the:",
+correct:"Fourth Thursday in November",
+incorrect:["Third Thursday in November", "November 26 each year"]};
 
-var question2 = {question:"What is my name?",
-correct:"Elisheva",
-incorrect:["Bob", "Charlie", "Lara"]};
+var question2 = {question:"2. The first Thanksgiving lasted:",
+correct:"3 days",
+incorrect:["1 day", "2 days"]};
 
-var question3 = {question:"What is my name?",
-correct:"Elisheva",
-incorrect:["Bob", "Charlie", "Lara"]};
+var question3 = {question:"3. Which of the following was NOT served at the Pilgrims Thanksgiving meal?",
+correct:"Cranberries, corn, and mashed potatoes",
+incorrect:["Rabbit, chicken, wild turkey, and dried fruit", "Venison, fish, goose"]};
 
-var question4 = {question:"What is my name?",
-correct:"Elisheva",
-incorrect:["Bob", "Charlie", "Lara"]};
+var question4 = {question:"4. Which Indian tribe taught the Pilgrims how to cultivate the land and were invited to the Thanksgiving meal?",
+correct:"Wampanoag",
+incorrect:["Apache", "Cherokee"]};
 
-var question5 = {question:"What is my name?",
-correct:"Elisheva",
-incorrect:["Bob", "Charlie", "Lara"]};
+var question5 = {question:"5. Approximately how many turkeys are eaten each year on Thanksgiving in the United States??",
+correct:"280 million",
+incorrect:["500 million", "100 million"]};
 
-var question6 = {question:"What is my name?",
-correct:"Elisheva",
-incorrect:["Bob", "Charlie", "Lara"]};
+var question6 = {question:"6. Which southern state was the first to adopt a Thanksgiving Day in 1855?",
+correct:"Virginia",
+incorrect:["South Carolina", "Georgia"]};
 
-var question7 = {question:"What is my name?",
-correct:"Elisheva",
-incorrect:["Bob", "Charlie", "Lara"]};
+var question7 = {question:"7. What utensil was not used by the Pilgrims to eat Thanksgiving dinner?",
+correct:"Fork",
+incorrect:["Knife", "Spoon"]};
 
+var question8 = {question:"8. What is a baby turkey called?",
+correct:"A poult",
+incorrect:["A chick", "A nestling"]};
 
-var classArray = ["c", "i", "i", "i"];
+var question9 = {question:"9. It has been estimated that how many Americans eat turkey at Thanksgiving?",
+correct:"88%",
+incorrect:["50%", "75%"]};
+
+var question10 = {question:"10. A full grown turkey has about how many feathers?",
+correct:"3,500",
+incorrect:["A million", "Too many to count!"]};
+
+var array = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10]
+console.log(array);
+
+// var classArray = ["c", "i", "i", "i"];
 
 var form = $("form");
 
-for (var j = 0; j < 7; j++) {
+for (var j = 0; j < array.length; j++) {
 
 // the question
-var quest = $("<p>").text((question1).question);
+var quest = $("<p>").text(array[j].question);
 var section = $("<section>");
 
 
@@ -48,44 +62,40 @@ form.append(quest);
 
 
 //do again for other incorrect choices
-for (var i = 0; i <= question1.incorrect.length; i++) {
+for (var i = 0; i < 3; i++) {
 
 	var radioButton = $("<div>");
-// section.append(radioButton);
-if (i == 0){
-	radioButton.addClass("radio corr");
+
 	label = $("<label>");
 	input = $("<input>");
 	input.attr("type", "radio");
-	input.attr("name", "optradio" + (i+1));
-	input.attr("id", "question"+(j+1)+"choice1");
-
-	label.append(input);
-
-	label.append(question1.correct);
-	radioButton.append(label)
-}
-
-else{
-	
-	radioButton = $("<div>");
-
-	radioButton.addClass("radio incorr");
-	label = $("<label>");
-	input = $("<input>");
-	input.attr("type", "radio");
-	input.attr("name", "optradio" + (i+1));
+	input.attr("name", "optradio" + (j+1));
 	input.attr("id", "question"+(j+1)+"choice" + (i+1));
 
 	label.append(input);
 
-	
+// section.append(radioButton);
+if (i == 0){
 
-	label.append(question1.incorrect[i-1]);
+	radioButton.addClass("radio corr");
+	// label = $("<label>");
+	// input = $("<input>");
+	// input.attr("type", "radio");
+	// input.attr("name", "optradio" + (i+1));
+	// input.attr("id", "question"+(j+1)+"choice1");
 
-	
-	radioButton.append(label)
+	// label.append(input);
+
+	label.append(array[j].correct);
+	// radioButton.append(label)
 }
+
+else{
+	radioButton.addClass("radio incorr");
+	label.append(array[j].incorrect[i-1]);
+}
+radioButton.append(label)
+
 
 	//randomize the order of the choices
 	var rand = Math.floor(Math.random()*2);
@@ -115,7 +125,7 @@ function decrement() {
 
 	secondsLeft--;
 
-	$("#show-number").html("<h2>" + secondsLeft + "</h2>");
+	$("#show-number").html("<h2> Time Left: " + secondsLeft + " Seconds</h2>");
 
 	if (secondsLeft === 0) {
 
@@ -129,9 +139,81 @@ function decrement() {
     }
 }
 
+function calculateResults(){
+
+	
+	var counter = 0;
+	var incorrectCounter = 0;
+
+for (var i = 0; i < array.length; i++) {
+	var x = document.getElementById("question"+(i+1)+"choice1").checked;
+  var y = document.getElementById("question"+(i+1)+"choice2").checked;
+  var z = document.getElementById("question"+(i+1)+"choice3").checked;
+    if (x){
+    	counter++
+		}
+
+		//calculate how many were answered incorrectly so 
+		// can see how many unanswered
+		else if ((y)||(z)){
+			incorrectCounter++
+		}
+	
+	}
+
+console.log("counter: " + counter);
+console.log("IncorrectCounter: " + incorrectCounter);
+
+displayResults(counter, incorrectCounter)
+
+// 	var results = $("<section class='results'>");
+// 	numCorrect = $("<h2 class='numCorrect'>");
+// 	numCorrect.html("Correct: " + counter);
+// 	numIncorrect = $("<h2 class='numIncorrect'>");
+// 	numIncorrect.html("Incorrect: " + (incorrectCounter));
+// 	numUnanswered = $("<h2 class='numUnanswered'>");
+// 	numUnanswered.html("Unanswered: " + (array.length - counter - incorrectCounter));
+// 	results.append(numCorrect, numIncorrect, numUnanswered);
+// 	$(".display-info").html(results);
+// 	console.log(numIncorrect);
+// displayResults();
+// return counter
+
+};
+
+function displayResults(right, wrong){
+	// var results = $("<section class='results'>");
+	// numCorrect = $("<div class='numCorrect'>");
+	// numCorrect.html("Correct: " + counter);
+	// numIncorrect = $("<div class='numIncorrect'>");
+	// numIncorrect.html("Incorrect: " + array.length-numCorrect);
+	// results.append(numCorrect, numIncorrect);
+	// $(".container").append(results);
+
+
+var results = $("<section class='results'>");
+	numCorrect = $("<h2 class='numCorrect'>");
+	numCorrect.html("Correct: " + right);
+	numIncorrect = $("<h2 class='numIncorrect'>");
+	numIncorrect.html("Incorrect: " + (wrong));
+	numUnanswered = $("<h2 class='numUnanswered'>");
+	numUnanswered.html("Unanswered: " + (array.length - right - wrong));
+	results.append(numCorrect, numIncorrect, numUnanswered);
+	$(".display-info").html(results);
+	console.log(numIncorrect);
+
+
+}
+
 function stop() {
+	calculateResults();
 
 	clearInterval(intervalId);
+	// var windowTimeout = setTimeout(function(){
+ //        $(".container").empty();
+ //        // displayResults();
+ //      }, 1000);
+
 }
 
 run();
